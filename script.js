68a120099aa7a74f4d41c3b764fbcd10999f7112
Jack Fischer <jrfisch95@gmail.com>
2019-02-17 23:08:12 -0500

    Finished soldier programs section, added form selection, added images for this year's soldier profiles. Still need to add last year's soldier profiles, update the forms with correct inputs, and update intro.

---
+++ script.js
@@ -0,0 +1,14 @@
+
+
+//Form selector
+$(document).on('change', '.volunteerFormSelector', function() {
+  let target = $(this).data('target');
+  let show = $('option:selected', this).data('show');
+  $(target).addClass('d-none');
+  $(show).removeClass('d-none');
+});
+
+//On load, unhide default form
+$(document).ready(() => {
+    $('.volunteerFormSelector').trigger('change');
+  });
